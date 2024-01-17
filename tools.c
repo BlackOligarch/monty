2a2521cddc42e1c5848122e97c98eea6e64374dc
BlackOligarch <godfred.hammond2018@yahoo.com>
2024-01-17 14:32:17 +0000

    used a switch statemtnts for the call_fun

--- tools.c
+++ tools.c
@@ -134,26 +134,34 @@ void call_fun(op_func func, char *op, char *val, int ln, int format)
 	int flag = 1;
 	int i = 0;
 
-	if (strcmp(op, "push") == 0)
+	switch (strcmp(op, "push"))
 	{
-		if (val != NULL && val[0] == '-')
-		{
-			val = val + 1;
-			flag = -1;
-		}
-		if (val == NULL)
-			err(5, ln);
-		for (i = 0; val[i] != '\0'; i++)
-		{
-			if (isdigit(val[i]) == 0)
+		case 0:
+			if (val != NULL && val[0] == '-')
+			{
+				val += 1;
+				flag = -1;
+			}
+			if (val == NULL)
 				err(5, ln);
-		}
-		node = create_node(atoi(val) * flag);
-		if (format == 0)
-			func(&node, ln);
-		if (format == 1)
-			add_to_queue(&node, ln);
+			for (i = 0; val[i] != '\0'; i++)
+			{
+			if (!isdigit(val[i]))
+				err(5, ln);
+			}
+
+				node = create_node(atoi(val) * flag);
+				switch (format)
+				{
+				case 0:
+				func(&node, ln);
+				break;
+				case 1:
+				add_to_queue(&node, ln);
+				break;
+				}
+				break;
+				default:
+				func(&head, ln);
+				}
 	}
-	else
-		func(&head, ln);
-}
